7f9d3a9b3393186fc2e85bb39898d85d702dbb4b
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-08-02 15:09:32 +0300

    Feat: trade activity layout

--- app/src/main/java/com/github/bggoranoff/tradegame/TradeActivity.java
+++ app/src/main/java/com/github/bggoranoff/tradegame/TradeActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class TradeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
